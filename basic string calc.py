547f7d0b748749f778b2c9c6af6ef54223b1af0b
moses <esotericgravity@gmail.com>
2018-09-14 17:27:05 -0400

    Add files via upload

---
+++ basic string calc.py
@@ -0,0 +1,34 @@
+def parse_calc(str):
+    for i in range(len(str)):
+        if str[i]=='+':
+            try:
+                a=int(str[:i]) + int(str[i+1:])
+                print(int(str[:i]))
+            except ValueError:
+                a="invalid, string cannot be converted to integer."
+            return a
+        if str[i]=='-':
+            try:
+                a=int(str[:i]) + int(str[i+1:])
+                print(int(str[:i]))
+            except ValueError:
+                a="invalid, string cannot be converted to integer."
+            return a
+        if str[i]=='*':
+            try:
+                a=int(str[:i]) * int(str[i+1:])
+                print(int(str[:i]))
+            except ValueError:
+                a="invalid, string cannot be converted to integer."
+            return a
+        if str[i]=='/':
+            try:
+                a=int(str[:i]) / int(str[i+1:])
+                print(int(str[:i]))
+            except ValueError:
+                a="invalid, string cannot be converted to integer."
+            return a
+    return "invalid, string does not contain an operator"
+
+if __name__ == '__main__':
+    print(parse_calc(""))
